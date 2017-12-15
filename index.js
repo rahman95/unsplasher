160a7bdefc3f51084e3a011b48bd9e61f81843ab
Rahman Younus <rahman_95@live.co.uk>
2017-12-15 16:23:22 +0000

    removed wrapper api and using actual unsplash api

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 require('dotenv').config();
+import Unsplash from 'unsplash-js';
 const wallpaper = require('wallpaper');
-const unsplash = require('unsplash-api');
+const unsplash = new Unsplash({
+  applicationId: process.env.CLIENT_ID,
+  secret: process.env.API_SECRET,
+  callbackUrl: process.env.CALLBACK_URL
+});
 
-console.log('Connecting to Unsplashed.');
-unsplash.init(process.env.CLIENT_ID);
+unsplash.photos.getRandomPhoto(
+    {
+     height: process.env.HEIGHT || 1080,
+     width: process.env.WIDTH || 1920,
+     featured: process.env.FEATURED || true,
+    }
+).then(toJson)
+.then(json => {
+    // Your code
+  });
+
 
 
 // wallpaper.set('unicorn.jpg').then(() => {
