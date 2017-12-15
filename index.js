5fbf451d3418f54771e07ec0ccd477f66edd33d8
Rahman Younus <rahman_95@live.co.uk>
2017-12-15 23:38:12 +0000

    got images downloading

--- index.js
+++ index.js
@@ -8,13 +8,18 @@ const unsplash = new Unsplash({
   secret: process.env.API_SECRET,
   callbackUrl: process.env.CALLBACK_URL
 });
+const dir = __dirname + '/downloads/';
+var fs = require('fs'),
+    https = require('follow-redirects').https,
+    request = require('request'),
+    mime = require('mime-types');
 
 var getRandomImage = new Promise(
     function (resolve, reject) {
         unsplash.photos.getRandomPhoto({
-            height: process.env.HEIGHT || 1080,
-            width: process.env.WIDTH || 1920,
-            featured: process.env.FEATURED || true,
+            'height': process.env.HEIGHT || 1080,
+            'width': process.env.WIDTH || 1920,
+            'featured': process.env.FEATURED || true,
         }).then(toJson).then(image => {
             if(image){
                 var randomImage = {
@@ -35,10 +40,20 @@ var getRandomImage = new Promise(
     }
 );
 
+var download = function(image) {
+    var request = https.get(image.download, function(response) {
+        var file = fs.createWriteStream(dir + image.id + '.' + mime.extension(response.headers['content-type']));
+        response.pipe(file);
+        file.on('finish', function() {
+            file.close();
+        });
+    });
+};
+
+
 var getImageDetails = function () {
-    getRandomImage.then(function (image) {
-        return image;
-    })
+    getRandomImage
+    .then(download)
     .catch(function (error) {
         console.log(error.message);
     });
